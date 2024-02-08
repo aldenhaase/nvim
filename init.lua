4cfea135ddd8416e4068bae2c50b7f42510ce5fb
aldenhaase <aldenhaase@gmail.com>
2024-02-07 17:38:39 -0800

    Feature: update listchars

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@ require("setup")
 
 local set = vim.opt
 
+set.listchars = {
+    trail = '·',
+    precedes = '«',
+    extends = '»',
+    eol = '↲',
+    tab = '▸\\'};
 set.tabstop = 4;
 set.softtabstop = 4;
 set.shiftwidth = 4;
