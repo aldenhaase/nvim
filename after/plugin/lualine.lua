d41327028cc3096bbf4d81ca17651bdf07612906
aldenhaase <aldenhaase@gmail.com>
2025-01-17 14:06:31 -0800

    Feature: theme

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,53 +1,32 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
+local colors = require("cyberdream.colors").default
+local opts = require("cyberdream.config").options
 
--- stylua: ignore
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
+if opts.transparent then
+    colors.bg = "NONE"
+end
 
 require('lualine').setup {
   options = {
-    theme = bubbles_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
+    theme = cyberdream,
+    component_separators = '',
+    section_separators = {},
   },
   sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
-    },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
+    lualine_a = {'mode'},
+    lualine_b = {{ 'filename', path = 1}},
+    lualine_c = {},
+    lualine_x = { 'branch'},
+    lualine_y = {{ 'diff',
+                    symbols    = { added = " ",
+                                   modified = " ",
+                                   removed = " "
+                                 },
+                    diff_color = { added    = { fg = colors.green },
+                                   modified = { fg = colors.orange },
+                                   removed  = { fg = colors.red },
+                                 },
+                }},
+    lualine_z = {'location',},
   },
   inactive_sections = {
     lualine_a = { 'filename' },
